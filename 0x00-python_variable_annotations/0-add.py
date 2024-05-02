e2fdb4261825290391fc2875570e760f483a96b1
Blessed Oigbochie <eprince1800@gmail.com>
2024-05-02 13:45:45 +0100

    document task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
+"""rhis module has one functikn"""
+
+
 def add(a: float, b: float) -> float:
+    """rhis function sums 2 float nums
+    rwturns: a float of the sum.
+    """
     return a + b

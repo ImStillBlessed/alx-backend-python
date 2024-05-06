6f63002939503ec730b3027346f088902872284b
Blessed Oigbochie <eprince1800@gmail.com>
2024-05-06 21:16:21 +0100

    documentation and typing 1 and 2

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int =10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
     This method waits a random number of seconds
     and prints the number
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,6 +8,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def measure_time(n: int, max_delay: int) -> float:
+    """.retuns tge total time for tge whole async"""
     start = time.time()
     await wait_n(n, max_delay)
     total_delay = time.time() - start

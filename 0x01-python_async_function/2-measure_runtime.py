245de004dd831470939ce4a2c1ced2136719f038
Oigbochie Blessed <eprince1800@gmail.com>
2024-07-24 20:30:15 +0100

    task 2 and 4

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -9,12 +9,11 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 async def measure_time(n: int, max_delay: int) -> float:
     """
-    Returns the average time taken for the whole async operation.
+    Returns the average time per call for the wait_n coroutine.
 
     Args:
         n (int): Number of times to spawn wait_n.
         max_delay (int): Maximum delay in seconds for each wait_n call.
-    
     Returns:
         float: The average time per call.
     """
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """this module contains 1 func"""
 
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Asynchronous coroutine that spawns wait_random n times
     with the specified max_delay.
